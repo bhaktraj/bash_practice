5e55e1a3aa791c169762b21af66874d2775ff910
bhaktraj <ankitbhaktraj@gmail.com>
2025-07-03 11:57:43 +0000

    System Scripts

---
+++ systeminfo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+echo 
+echo "###################### Disk Infromation ############################"
+
+echo
+
+df -h
+
+echo
+echo "##################### Ram Infromation  #############################"
+echo
+
+free -h
+
+echo
+echo "##################### Uptime of System #############################"
+echo
+uptime
+
+echo
